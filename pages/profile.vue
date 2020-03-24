df351a020e8e65182a8d6c094cf8699ae17b4711
Gatotz <gatot.prastyo@an.tv>
2020-03-24 16:22:02 +0700

    Belajar Nuxt dari awal: - Buat halaman profile - Buat halaman blog - Buat halaman detail

---
+++ pages/profile.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h2>Hello Beib!</h2>
+    <p>{{ message }}</p>
+    <input type="text" v-model="message">
+    <button @click="showAlert">Show Message</button>
+  </div>  
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message: ''
+    }
+  },
+  methods: {
+    showAlert (e) {
+      alert(this.message)
+    }
+  }
+}
+</script>
+
+
+
+
+<style>
+html,
+body {
+  padding: 0;
+  margin: 0;
+}
+body {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+h2 {
+  font-size: 48px;
+  color: #389acd;
+}
+p {
+  font-size: 18px;
+  color: #ababab;
+}
+</style>
